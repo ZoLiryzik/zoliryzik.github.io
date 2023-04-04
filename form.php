3e0bc8178774d586e4312a0fa7535e5ff0d1bd02
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2023-04-04 23:45:11 +0300

    Create form.php

---
+++ form.php
@@ -0,0 +1,5 @@
+<form action="send.php" method="post">
+<input type="text" name="fio" placeholder="Укажите ФИО" required>
+<input type="text" name="email" placeholder="Укажите e-mail" required>
+<input type="submit" value="Отправить">
+</form>
