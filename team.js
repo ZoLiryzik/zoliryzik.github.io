11c0d79aa222717735dcee5880982e14cb904454
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2023-07-06 01:58:20 +0300

    Update team.js

--- team.js
+++ team.js
@@ -10,7 +10,7 @@ const API = "https://discord-web-api.glitch.me/discord/user/";
 
 const owners = [
     {
-        "id": "1044668723195285558",
+        "id": "760400133212602369",
         "post": "Тот самый",
         "GHURL": "https://zoliryzik.github.io/developer/bio"
     }
