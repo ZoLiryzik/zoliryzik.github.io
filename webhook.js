676928bca1baa4722cf709a1f3d3e33bb3f22a7d
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-01 01:16:32 +0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,1 +1,1 @@
-const webhookURL = ${{ secrets.DISCORD_WEBHOOK_URL }}; // Этот URL будет заменен на значение секрета GitHub Actions
+const webhookURL = process.env.DISCORD_WEBHOOK_URL; // Этот URL будет заменен на значение секрета GitHub Actions
