9d299b534224d2c81e4dbd32fc986a864f26de7f
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-01 01:21:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,2 @@
-document.getElementById('myForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    
-    const data = {
-        name: name,
-        email: email
-    };
-    
-    // Сохранение данных в JSON-файл
-    const jsonData = JSON.stringify(data);
-    download(jsonData, 'data.json', 'application/json');
-});
-
-function download(content, fileName, contentType) {
-    const a = document.createElement("a");
-    const file = new Blob([content], { type: contentType });
-    a.href = URL.createObjectURL(file);
-    a.download = fileName;
-    a.click();
-}
+const mySecret = process.env.MY_SECRET;
+console.log('My Secret:', mySecret);
