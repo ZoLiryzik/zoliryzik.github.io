014293595893f77bc13f5343a3618bde3f5c16e2
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-09 21:34:18 +0300

    Delete updatefeedback.js

--- updatefeedback.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const feedbacks = JSON.parse(fs.readFileSync('feedback.json'));
-
-// Пример добавления отзыва
-feedbacks.push({ name: 'User', message: 'This is a sample feedback!' });
-
-fs.writeFileSync('feedback.json', JSON.stringify(feedbacks, null, 2));
