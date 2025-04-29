f6c50969b7973a234cc2f62f6b5742d3beb3c9e6
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-04-29 16:51:10 +0300

    Update otz.js

--- assets/js/otz.js
+++ assets/js/otz.js
@@ -13,6 +13,19 @@ $(document).ready(function() {
         }
     });
 
+    
+  const originalRepoURL = "https://zoliryzik.ru/";
+  if (window.location.href !== originalRepoURL) {
+    alert("⚠️ Этот сайт — копия! Оригинал: " + originalRepoURL + " "+ window.location.href);
+    // Или добавьте красный баннер на страницу
+    document.body.innerHTML += `
+      <div style="position: fixed; top: 0; background: red; color: white; padding: 10px; width: 100%; text-align: center;">
+        Это копия! Оригинал: <a href="${originalRepoURL}" style="color: white;">${originalRepoURL}</a>
+      </div>
+    `;
+  }
+
+    
     // Обработка отправки формы
     $('#bootstrapForm').submit(function(event) {
         event.preventDefault();
